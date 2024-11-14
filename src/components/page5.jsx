1361a3fa69c8f8d17813c3005283363c9191fd77
karthi <itsmecoder29@gmail.com>
2024-11-14 20:00:05 +0530

    new commit

--- src/components/page5.jsx
+++ src/components/page5.jsx
@@ -13,20 +13,25 @@ import CTS4 from "../assets/cts4.jpg"
 function page5(){
     return<>
         <div className="page5-main">
+
             <div className="page5-main-inner">
                 {/*card1*/}
+
                 <div className="page5-main-inner-outercard">
-                <div className="page5-main-inner-card">
-                    <div className="page5-main-inner-cardscroll">
-                        <img className="page5-main-inner-cards" src={T2}/>
-                        <img className="page5-main-inner-cards" src={T1}/>
-                        <img className="page5-main-inner-cards" src={T3}/>
-                        <img className="page5-main-inner-cards" src={T4}/>
+                    <div className="page5-main-inner-card">
+                        <div className="page5-main-inner-cardscroll">
+                            <img className="page5-main-inner-cards" src={T2}/>
+                            <img className="page5-main-inner-cards" src={T1}/>
+                            <img className="page5-main-inner-cards" src={T3}/>
+                            <img className="page5-main-inner-cards" src={T4}/>
+                        </div>
                     </div>
-                </div>
                     <div className="page5-main-inner-p1">
-                    <h3 className="page5-main-inner-p1-h3">Internship in STUST University Taiwan</h3>
-                    <p className="page5-main-inner-p1-p">Machine learning intern at Southern Taiwan University of Science and Technology, Taiwan. Engaged in hands-on learning and contributed to innovative projects in machine learning. Developed and implemented a spam detection project using text mining, significantly enhancing word detection and classification of spam messages</p>
+                        <h3 className="page5-main-inner-p1-h3">Internship in STUST University Taiwan</h3>
+                        <p className="page5-main-inner-p1-p">Machine learning intern at Southern Taiwan University of
+                            Science and Technology, Taiwan. Engaged in hands-on learning and contributed to innovative
+                            projects in machine learning. Developed and implemented a spam detection project using text
+                            mining, significantly enhancing word detection and classification of spam messages</p>
                     </div>
                 </div>
                 {/*card2*/}
@@ -41,7 +46,10 @@ function page5(){
                     </div>
                     <div className="page5-main-inner-p1">
                         <h3 className="page5-main-inner-p1-h3">Cognizant Technoverse 2023 Winners</h3>
-                        <p className="page5-main-inner-p1-p">As the team leader, I guided our group to secure 1st place in the Cognizant Technoverse 2023 hackathon with our healthcare-focused project, Medigree. This achievement led to a one-month internship opportunity at Cognizant, where I gained valuable hands-on experience and expanded my professional <skills className=""></skills> </p>
+                        <p className="page5-main-inner-p1-p">As the team leader, I guided our group to secure 1st place
+                            in the Cognizant Technoverse 2023 hackathon with our healthcare-focused project, Medigree.
+                            This achievement led to a one-month internship opportunity at Cognizant, where I gained
+                            valuable hands-on experience and expanded my professional <skills className=""></skills></p>
                     </div>
                 </div>
                 {/*card3*/}
